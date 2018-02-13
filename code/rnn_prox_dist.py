63fcdc45fbaf54c88c777be43f72f4e89c953dd4
Fabian Schubert <fschubert@fias.uni-frankfurt.de>
2018-02-13 13:21:42 +0100

    .

--- code/rnn_prox_dist.py
+++ code/rnn_prox_dist.py
@@ -9,12 +9,12 @@ import pdb
 ###### Parameters
 
 ## Network
-N_e = 100 # number of excitatory neurons
+N_e = 200 # number of excitatory neurons
 N_i = int(N_e*0.2) # number of inhibitory neurons
 
 CF_ee = 0.1 # connection fraction E->E
 CF_ei = 0.2 # connection fraction I->E
-CF_ie = 1.0 # connection fraction E->I
+CF_ie = 0.5 # connection fraction E->I
 CF_ii = 0.0 # connection fraction I->I
 
 w_exc_min = 0.001
@@ -23,17 +23,26 @@ w_inh_max = -0.001
 
 ## Neuron
 g_neur = 100. # gain factor of the activation function
-r_target_e = 0.1 # homeostatic excitatory target firing rate
-r_target_i = 0.1 # homeostatic inhibitory target firing rate
+
+r_target_e_mu = 0.05 # mean homeostatic excitatory target firing rate
+r_target_e_sigm = 0.05 # standard deviation of homeostatic excitatory target firing rate
+r_target_set_e = np.minimum(1.,np.maximum(0.,np.random.normal(r_target_e_mu,r_target_e_sigm,N_e)))
+
+r_target_i_mu = 0.1 # mean homeostatic inhibitory target firing rate
+r_target_i_sigm = 0.05 # standard deviation of homeostatic inhibitory target firing rate
+r_target_set_i = np.minimum(1.,np.maximum(0.,np.random.normal(r_target_i_mu,r_target_i_sigm,N_i))) 
+
 mu_IP = 0.001 # threshold adaption rate
+
 T_e_max_init = 1.
 T_i_max_init = 1.
+
 mu_mem_noise = 0.
-sigm_mem_noise = np.sqrt(0.04)
+sigm_mem_noise = 0.#np.sqrt(0.04)
 ##
 
 ## Synaptic Normalization
-w_total_ee = 1. # total presynaptic E->E input
+w_total_ee = .5 # total presynaptic E->E input
 w_total_ei = -1. # total presynaptic I->E input
 w_total_ie = 1. # total presynaptic E->I input
 w_total_ii = 0. # total presynaptic I->I input
@@ -48,7 +57,9 @@ mu_plast_ei = 0.01 # I->E learning rate
 ##
 
 ## Simulation
-n_t = 5000 # simulation time steps
+n_t = 15000 # simulation time steps
+n_t_skip_w_rec = 10 # only record every n_th weight matrix
+n_t_w_rec = int(n_t/n_t_skip_w_rec)
 ##
 
 ######
@@ -143,8 +154,8 @@ def main():
 	x_e_rec = np.ndarray((n_t,N_e))
 	x_i_rec = np.ndarray((n_t,N_i))
 
-	W_ee_rec = np.ndarray((n_t,N_e,N_e))
-	W_ei_rec = np.ndarray((n_t,N_e,N_i))
+	W_ee_rec = np.ndarray((n_t_w_rec,N_e,N_e))
+	W_ei_rec = np.ndarray((n_t_w_rec,N_e,N_i))
 
 	T_e_rec = np.ndarray((n_t,N_e))
 	T_i_rec = np.ndarray((n_t,N_i))
@@ -173,13 +184,13 @@ def main():
 		##
 
 		## Update tresholds
-		T_e = update_th(T_e,x_e,r_target_e,mu_IP)
-		T_i = update_th(T_i,x_i,r_target_i,mu_IP)
+		T_e = update_th(T_e,x_e,r_target_set_e,mu_IP)
+		T_i = update_th(T_i,x_i,r_target_set_i,mu_IP)
 		##
 		
 		## Update weights
 		W_ee = hebb_ee(W_ee,x_e,x_e_old,mu_plast_ee)
-		W_ei = hebb_ei(W_ei,x_e,x_i_old,r_target_e,mu_plast_ei)
+		W_ei = hebb_ei(W_ei,x_e,x_i_old,r_target_set_e,mu_plast_ei)
 
 		W_ee = synnorm(W_ee,w_total_ee)
 		W_ei = synnorm(W_ei,w_total_ei)
@@ -192,8 +203,9 @@ def main():
 		x_e_rec[t,:] = x_e[:]
 		x_i_rec[t,:] = x_i[:]
 
-		W_ee_rec[t,:,:] = W_ee[:,:]
-		W_ei_rec[t,:,:] = W_ei[:,:]
+		if t%n_t_skip_w_rec == 0:
+			W_ee_rec[int(t/n_t_skip_w_rec),:,:] = W_ee[:,:]
+			W_ei_rec[int(t/n_t_skip_w_rec),:,:] = W_ei[:,:]
 
 		T_e_rec[t,:] = T_e[:]
 		T_i_rec[t,:] = T_i[:]
@@ -201,6 +213,28 @@ def main():
 		I_ee_rec[t,:] = I_ee[:]
 		I_ei_rec[t,:] = I_ei[:]
 		##
+
+	fig_sp, ax_sp = plt.subplots(1,1)
+	ax_sp.pcolormesh(x_e_rec[-800:,:].T,cmap="gray_r")
+	ax_sp.set_xlabel("Time Step + 4200")
+	ax_sp.set_ylabel("Exc. Neuron #")
+
+	fig_mean_x, ax_mean_x = plt.subplots(1,1)
+	ax_mean_x.plot(x_e_rec.mean(axis=1),label="Excitatory Population")
+	ax_mean_x.plot(x_i_rec.mean(axis=1),label="Inhibitory Population")
+	ax_mean_x.legend()
+	ax_mean_x.set_xlabel("Time Step")
+	ax_mean_x.set_ylabel("Mean Rate")
+
+	fig_T,ax_T = plt.subplots(1,1)
+	ax_T.plot(T_e_rec.mean(axis=1),label="Excitatory Population")
+	ax_T.plot(T_i_rec.mean(axis=1),label="Inhibitory Population")
+	ax_T.legend()
+	ax_T.set_xlabel("Time Step")
+	ax_T.set_ylabel("Mean Treshold")
+	
+
+	plt.show()
 
 
 	## See what we got
