84e5c8c601d260b63c6dd4be1cb52873680f5779
FabianSchubert <fj.schubert@freenet.de>
2018-05-18 11:22:42 +0200

    .

---
+++ code/rnn_prox_dist_non_bin/cpp_code/parameters.hpp
@@ -0,0 +1,22 @@
+namespace parameters
+{	
+	// activation function
+    const double m_0 = 0.5;
+	const double a_m = 0.5;
+	const double theta_m = 0.5;
+	const double s_m = 0.05;
+
+	const double a_t = 0.33;
+	const double theta_t = 0.5;
+	const double s_t = 0.1;
+
+	const double theta_p = 0.5;
+	const double s_p = 0.05;
+
+	// simulation
+
+	const int n_t = 1000;
+	const double mu_learn = 0.01;
+	const double mu_mean_act = 0.01;
+
+}
