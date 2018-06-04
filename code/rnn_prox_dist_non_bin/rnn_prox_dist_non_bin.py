83a89d79574fb62e6319e5f3b16c8cf68a0f7b40
FabianSchubert <fj.schubert@freenet.de>
2018-06-05 00:39:23 +0200

    .

--- code/rnn_prox_dist_non_bin/rnn_prox_dist_non_bin.py
+++ code/rnn_prox_dist_non_bin/rnn_prox_dist_non_bin.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import matplotlib.pyplot as plt
-from tqdm import tqdm
+#import matplotlib.pyplot as plt
+#from tqdm import tqdm
 
-import pdb
+#import pdb
 
 from grammar_data_reader import *
 
